c375476327ab0216f6f5ec998375563e0fee0092
Ravi Shankar <wafflespeanut@gmail.com>
2017-08-15 18:09:17 -0700

    Create droplets

--- kubernetes/setup-nodes.py
+++ kubernetes/setup-nodes.py
@@ -1,26 +1,14 @@
-import getpass, json, os, requests
+import getpass, json, os, requests, time
 
 from argparse import ArgumentParser
 
 CONFIG_FILE = 'config.json'
 
-NODE_CREATION_REQUEST = {
-    'name': None,
-    'region': None,
-    'size': None,
-    'image': 'coreos-stable',
-    'ssh_keys': [],
-    'backups': False,
-    'ipv6': True,
-    'user_data': None,
-    'private_networking': True,
-    'volumes': [],
-    'tags': [],
-}
-
 class DigitalOceanKubeRunner(object):
     root_url = 'https://api.digitalocean.com/v2'
     ssh_url = root_url + '/account/keys'
+    region_url = root_url + '/regions'
+    droplet_url = root_url + '/droplets'
     headers = {
         'Content-Type': 'application/json'
     }
@@ -32,6 +20,21 @@ class DigitalOceanKubeRunner(object):
         with open(public_key_path, 'r') as fd:
             self.pkey = fd.read().strip()
 
+    def node_creation_request(self, name, region, size, ssh_key_id):
+        return {
+            'name': name,
+            'region': region,
+            'size': size,
+            'image': 'coreos-stable',
+            'ssh_keys': [ ssh_key_id ],
+            'backups': False,
+            'ipv6': True,
+            'user_data': None,
+            'private_networking': True,
+            'volumes': [],
+            'tags': [],
+        }
+
     def _request(self, method, url, data=None):
         if data is not None:
             data = json.dumps(data)
@@ -41,7 +44,8 @@ class DigitalOceanKubeRunner(object):
         data, code = resp.text, resp.status_code
 
         if code < 200 or code >= 300:
-            print 'Got a %s response: %s' % (code, data)
+            print 'Got %s response: %s' % (code, data)
+            raise Exception('Boo!')
         return json.loads(data)
 
     def create_or_use_public_key(self):
@@ -63,9 +67,49 @@ class DigitalOceanKubeRunner(object):
         print 'Using key: %s (fingerprint: %s)' % (key['name'], key['fingerprint'])
         return key['id']
 
+    def get_regions(self):
+        data = self._request('GET', self.region_url)
+        self.regions = filter(lambda r: r['available'], data['regions'])
+
+    def get_droplets(self):
+        data = self._request('GET', self.droplet_url)
+        self.droplets = data['droplets']
+
+    def get_or_create_node(self, ssh_key_id, node, node_id=0):
+        size = self.config['size']
+        regions = filter(lambda r: size in r['sizes'], self.regions)
+        if not regions:
+            exit('No regions available for size %s' % size)
+
+        region = regions[0]['slug']
+        name = 'coreos-%s-%s-%s' % (node, node_id, region)
+        droplet = filter(lambda d: d['name'] == name, self.droplets)
+        if droplet:
+            print 'Re-using existing droplet...'
+            return droplet[0]['id']
+
+        payload = self.node_creation_request(name, region, size, ssh_key_id)
+        print 'Creating droplet %s with size %s...' % (name, size)
+        data = self._request('POST', self.droplet_url, payload)
+        droplet_id = data['droplet']['id']
+        status = data['droplet']['status']
+
+        print 'Waiting for droplet...'
+        url = self.droplet_url + '/' + str(droplet_id)
+        while status != 'active':
+            time.sleep(5)
+            data = self._request('GET', url)
+            status = data['droplet']['status']
+
+        self.droplets.append(data['droplet'])
+        return droplet_id
+
     def start(self):
         ssh_key_id = self.create_or_use_public_key()
-
+        self.get_regions()
+        self.get_droplets()
+        master_id = self.get_or_create_node(ssh_key_id, 'master')
+        worker_id = self.get_or_create_node(ssh_key_id, 'worker')
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Deploy kubernetes in Digital Ocean.')
