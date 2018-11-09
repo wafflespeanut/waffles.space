8b645a9b745a4e289d3f500aeadac55582b0c303
Ravi Shankar <wafflespeanut@gmail.com>
2018-11-09 21:04:53 +0530

    TLS support (afte an EON!)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,34 +1,32 @@
 #!/usr/bin/bash
 
-ALIAS_ADDR="10.0.0.10"
-
-# `localhost` and `0.0.0.0` conflicts with docker containers. It's better
-# to alias the IP address (that's what Kubernetes does).
-sudo ifconfig eth0:1 ${ALIAS_ADDR} netmask 255.255.255.0 up
+docker network create waffles
 
 echo 'Launching static file server...'
 docker run --name static \
     --restart always \
+    --network waffles \
     -v /home/core/source:/source \
     -v /home/core/private:/private \
     -v /home/core/config:/config \
-    -p ${ALIAS_ADDR}:8000:8000 \
     -e CUSTOM_4XX=/source/4xx.html \
     -e SOURCE=/source \
     -e PRIVATE_SOURCE=/private \
     -e CONFIG=/config/static_server_config.json -d \
     wafflespeanut/static-server
 
-echo 'Deploying Nginx proxy...'
-docker run --name nginx \
-    --restart always \
-    -v /home/core/config/nginx.conf:/etc/nginx/nginx.conf \
-    -v /home/core/config/default.conf:/etc/nginx/conf.d/default.conf \
-    -p 80:80 -d \
-    nginx:1.13-alpine
-
 echo 'Launching ASCII art generator...'
 docker run --name ascii-gen \
     --restart always \
-    -p ${ALIAS_ADDR}:5000:5000 -d \
-    wafflespeanut/ascii-gen
+    --network waffles \
+    -d wafflespeanut/ascii-gen
+
+echo 'Deploying Nginx proxy...'
+docker run --name nginx \
+    --restart always \
+    --network waffles \
+    -v /home/core/config/nginx.conf:/etc/nginx/nginx.conf \
+    -v /home/core/config/default.conf:/etc/nginx/conf.d/default.conf \
+    -v /home/core/letsencrypt/live/waffles.space:/etc/certs \
+    -v /home/core/letsencrypt/archive:/archive \
+    -p 80:80 -p 443:443 -d nginx:alpine
